c7a353a3e11c2d10f73c4846fe99a8f1dc4eedf2
unknown <milesjoh001@warren.k12.in.us>
2019-01-11 13:30:23 -0500

    notes

---
+++ Variables/lesson1.js
@@ -0,0 +1,35 @@
+/*
+Scope
+
+Global Varibales - variables can be used throughout the entire document/script
+
+Local Variables - the variable can only be used in the section it was defined
+
+
+*/
+
+//ACTIVE
+//********************NOT NEEDED FOR HTML TRIGGERING*******/
+$(document).ready(function(){
+
+    $('#button').click(function(){
+        showInfo();
+    })
+
+})
+//**********************/
+//DEFINITIONS
+var userName
+
+
+
+function getInfo(){
+    userName=('#userName').val();
+}
+
+function showInfo(){
+    getInfo();
+    var message ='Hello' + userName +',glad to meet you!';
+    
+    $('#output').text(message).show().animate({'top':'opx','opacity':'1'} 'slow');
+}
